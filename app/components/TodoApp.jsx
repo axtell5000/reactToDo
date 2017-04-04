0a69c20a8c66f97815007ec1ecdc33224f419f65
Stephen Axtell <axtell.stephen@gmail.com>
2017-04-04 12:26:08 +0200

    Add new test files

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,39 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 //Is going to maintain state
 var TodoApp = React.createClass({
+
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        },
+        {
+          id: 3,
+          text: 'Feed the cat'
+        },
+        {
+          id: 4,
+          text: 'Wash the dishes'
+        }
+
+      ]
+    }
+  },
+
   render: function () {
+
+    var {todos} = this.state; //getting from the state
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     );
   }
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+
+  render: function () {
+
+   var {todos} = this.props;
+   var renderTodos = () => {
+     return todos.map((todo)  => {
+       //key is important, each component has to have an unique id
+       return (
+         <Todo key={todo.id} {...todo}/>
+       );
+     });
+   };
+
+   return (
+     <div>
+       {renderTodos()}
+     </div>
+   )
+  }
+
+});
+
+
+module.exports = TodoList;
