e456be2a794048c8e101852151dddf36940f6c6d
Stephen Axtell <axtell.stephen@gmail.com>
2017-04-05 18:01:35 +0200

    Add new todo search files

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,12 +1,16 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
-//Is going to maintain state
+//This is the only component that maintain state, the rest are for presentation and user interactive responses,
+//Try keep the number of components that maintain state to a minimum
 var TodoApp = React.createClass({
 
   getInitialState: function () {
     return {
+      showCompleted: false,
+      searchText: '',
       todos: [
         {
           id: 1,
@@ -32,12 +36,18 @@ var TodoApp = React.createClass({
   handleAddTodo: function (text) {
     alert('new todo: ' + text);
   },
-
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   render: function () {
 
     var {todos} = this.state; //getting from the state
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
